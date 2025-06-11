96df25b8370897f645e01ae8c7f33d2f58916ac4
lint69 <ljovic009@gmail.com>
2025-06-11 22:44:54 +0200

    Add base so bot can be started

--- Program.cs
+++ Program.cs
@@ -1,5 +1,54 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using DotNetEnv;
+using Discord.Commands;
 
-namespace Bot;
+class Program
+{
+    private static DiscordSocketClient? client;
 
+    static async Task Main()
+    {
+        Env.Load();
+        var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
 
+        var config = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+        client = new DiscordSocketClient(config);
+
+        client.MessageReceived += HandleMessageAsync;
+
+        client.Log += msg =>
+        {
+            Console.WriteLine(msg);
+            return Task.CompletedTask;
+        };
+
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private static async Task HandleMessageAsync(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        if (message is not SocketUserMessage userMessage) return;
+
+        int argPos = 0;
+        if (userMessage.HasStringPrefix("$", ref argPos))
+        {
+            var content = userMessage.Content.Substring(argPos).Trim();
+
+            if (content.Equals("ping", StringComparison.OrdinalIgnoreCase))
+            {
+                await message.Channel.SendMessageAsync($"pong! {client?.Latency ?? 0}ms");
+            }
+        }
+    }
+}
